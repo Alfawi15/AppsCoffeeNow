562f099fbea841f31bc578bf36bc440091cf1b94
Alfawi <Alfawi15@gmail.com>
2023-06-04 20:27:24 +0700

    commit

---
+++ app/src/main/java/com/example/appscoffeenow/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.appscoffeenow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button buttonLogin = findViewById(R.id.btnlgn);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = ((EditText) findViewById(R.id.email)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+                loginUser(email, password);
+            }
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(MainActivity.this, Home.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Authentication failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
